c4727bc336f997eca6ecc2509c6d7ec1273e680b
NowWeAre-Free <kontodarm@wp.pl>
2023-06-20 18:25:42 +0200

    ex 2 arrays

--- app.js
+++ app.js
@@ -21,3 +21,22 @@ const sum = () => {
 };
 
 //ŁOPATOLOGICZIE
+
+// Zadanie 2: Porównanie długości tablic
+
+// Mając  dwie tablice np. const firstArray = [1, 2, 3]; i const secondArray = [4, 5];,
+// sprawdź, która z nich jest dłuższa, a następnie wyświetl odpowiedni komunikat w konsoli, np. "Pierwsza tablica jest dłuższa" lub "Druga tablica jest dłuższa".
+// Jeśli tablice mają taką samą długość, wyświetl "Tablice mają taką samą długość".
+// Sprawdź czy Twojej rozwiązanie zadziała prawidłowo dla innych wariantów (czyli czy jest uniwersalne).
+
+const firstArray = [1, 2, 3];
+const secondArray = [4, 5];
+
+const whichArrayIsLonger = () => {
+  const first = firstArray.length;
+  const second = firstArray.length;
+
+  if (first > second) return "Pierwsza tablica jest dłuższa";
+  else if (second > first) return "Druga tablica jest dłuższa";
+  else return "Tablice mają taką samą długość";
+};
